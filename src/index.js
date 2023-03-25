0c99663a4d7d331ac4b9dbf2be84d60acb63c731
Eingress Ltd <scott@eingress.io>
2023-03-25 19:31:43 +0000

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,65 +1,64 @@
 import 'dotenv/config';
 
-const { GITHUB_API_TOKEN } = process.env;
-
-const baseURI = 'https://api.github.com';
+const { GITHUB_API_TOKEN, GITHUB_API_URI = 'https://api.github.com' } =
+  process.env;
 
 const FETCH_OPTIONS = {
   headers: { Authorization: `token ${GITHUB_API_TOKEN}` },
 };
 
-const mergeLeft = (a, b) => a.filter((e) => !b.includes(e));
+const arrayMergeLeft = (a, b) => a.filter((e) => !b.includes(e));
+
+const getFollowers = async (page = 1) => {
+  const res = await fetch(
+    `${GITHUB_API_URI}/user/followers?page=${page}&per_page=100`,
+    FETCH_OPTIONS,
+  );
+
+  if (res.ok) {
+    const data = await res.json();
+    data.map((user) => followers.push(user.login));
+
+    let link = res.headers.get('link');
+    if (link.includes(`rel=\"next\"`)) {
+      await getFollowers(++page);
+    }
+  }
+};
+
+const getFollowing = async (page = 1) => {
+  const res = await fetch(
+    `${GITHUB_API_URI}/user/following?page=${page}&per_page=100`,
+    FETCH_OPTIONS,
+  );
+
+  if (res.ok) {
+    const data = await res.json();
+    data.map((user) => following.push(user.login));
+
+    let link = res.headers.get('link');
+    if (link.includes(`rel=\"next\"`)) {
+      await getFollowing(++page);
+    }
+  }
+};
+
+const deleteFollower = async (user) => {
+  console.log(`Unfollowing: ${user}…`);
+  await fetch(`${GITHUB_API_URI}/user/following/${user}`, {
+    ...FETCH_OPTIONS,
+    method: 'DELETE',
+  });
+};
+
+let followers = [];
+let following = [];
+let rejects = [];
 
 (async () => {
-  let followers = [];
-  let following = [];
-  let rejects = [];
-
-  const getFollowers = async (page = 1) => {
-    const res = await fetch(
-      `${baseURI}/user/followers?page=${page}&per_page=100`,
-      FETCH_OPTIONS,
-    );
-
-    if (res.ok) {
-      const data = await res.json();
-      data.map((user) => followers.push(user.login));
-
-      let link = res.headers.get('link');
-      if (link.includes(`rel=\"next\"`)) {
-        await getFollowers(++page);
-      }
-    }
-  };
-
-  const getFollowing = async (page = 1) => {
-    const res = await fetch(
-      `${baseURI}/user/following?page=${page}&per_page=100`,
-      FETCH_OPTIONS,
-    );
-
-    if (res.ok) {
-      const data = await res.json();
-      data.map((user) => following.push(user.login));
-
-      let link = res.headers.get('link');
-      if (link.includes(`rel=\"next\"`)) {
-        await getFollowing(++page);
-      }
-    }
-  };
-
-  const deleteFollower = async (user) => {
-    console.log(`Unfollowing: ${user}…`);
-    await fetch(`${baseURI}/user/following/${user}`, {
-      ...FETCH_OPTIONS,
-      method: 'DELETE',
-    });
-  };
-
   await getFollowers();
   await getFollowing();
 
-  rejects = mergeLeft(following, followers);
+  rejects = arrayMergeLeft(following, followers);
   rejects.map(async (reject) => await deleteFollower(reject));
 })();
