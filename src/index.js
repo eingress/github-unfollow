e221fca66bb49bfbed985c87a6a833ea4d6d9767
Eingress Ltd <scott@eingress.io>
2023-06-26 22:35:23 +0100

    Use native test runner

--- src/index.js
+++ src/index.js
@@ -2,61 +2,77 @@ import 'dotenv/config';
 
 const { env, exit, stderr, stdout } = process;
 
-const { GITHUB_API_TOKEN, GITHUB_API_URI = 'https://api.github.com' } = env;
+const { GITHUB_API_TOKEN, GITHUB_API_URI = `https://api.github.com` } = env;
 
 const FETCH_OPTIONS = {
-  headers: { Authorization: `token ${GITHUB_API_TOKEN}` },
+  headers: {
+    Accept: `application/vnd.github+json`,
+    Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+  },
 };
 
 const arrayMergeLeft = (a, b) => a.filter((e) => !b.includes(e));
 
-const getFollowers = async (page = 1, result = []) => {
+const getFollowers = async (followers = [], page = 1) => {
   try {
     const response = await fetch(
       `${GITHUB_API_URI}/user/followers?page=${page}&per_page=100`,
       FETCH_OPTIONS,
     );
 
-    const data = await response.json();
-    data.map((user) => result.push(user.login));
+    const users = await response.json();
+    users.map((user) => followers.push(user.login));
 
     let link = response.headers.get('link');
     if (link.includes(`rel=\"next\"`)) {
-      await getFollowers(++page, result);
+      await getFollowers(followers, ++page);
     }
   } catch (error) {
     stderr.write(`${error}\n`);
     exit(1);
   }
-  return result;
+  return followers;
 };
 
-const getFollowing = async (page = 1, result = []) => {
+const getFollowees = async (followees = [], page = 1) => {
   try {
-    const res = await fetch(
+    const response = await fetch(
       `${GITHUB_API_URI}/user/following?page=${page}&per_page=100`,
       FETCH_OPTIONS,
     );
 
-    const data = await res.json();
-    data.map((user) => result.push(user.login));
+    const users = await response.json();
+    users.map((user) => followees.push(user.login));
 
-    let link = res.headers.get('link');
+    let link = response.headers.get('link');
     if (link.includes(`rel=\"next\"`)) {
-      await getFollowing(++page, result);
+      await getFollowees(followees, ++page);
     }
   } catch (error) {
     stderr.write(`${error}\n`);
     exit(1);
   }
-  return result;
+  return followees;
 };
 
-const deleteFollower = async (user) => {
+const followUser = async (user) => {
   try {
     await fetch(`${GITHUB_API_URI}/user/following/${user}`, {
       ...FETCH_OPTIONS,
-      method: 'DELETE',
+      method: `PUT`,
+    });
+    stdout.write(`Followed: ${user}\n`);
+  } catch (error) {
+    stderr.write(`${error}\n`);
+    exit(1);
+  }
+};
+
+const unfollowUser = async (user) => {
+  try {
+    await fetch(`${GITHUB_API_URI}/user/following/${user}`, {
+      ...FETCH_OPTIONS,
+      method: `DELETE`,
     });
     stdout.write(`Unfollowed: ${user}\n`);
   } catch (error) {
@@ -66,9 +82,14 @@ const deleteFollower = async (user) => {
 };
 
 (async () => {
-  let followers = await getFollowers();
-  let following = await getFollowing();
-  let deletable = arrayMergeLeft(following, followers);
+  const followees = await getFollowees();
+  const followers = await getFollowers();
 
-  deletable.map(async (user) => await deleteFollower(user));
+  arrayMergeLeft(followers, followees).map(
+    async (user) => await followUser(user),
+  );
+
+  arrayMergeLeft(followees, followers).map(
+    async (user) => await unfollowUser(user),
+  );
 })();
